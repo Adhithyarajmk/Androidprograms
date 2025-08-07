bfbbe2f6115c2a727f72fddb93005299f8f9bb6a
Adhithyarajmk <adhithyaraj25@gmail.com>
2025-08-07 15:51:47 +0530

    Androidprograms

---
+++ Intent5/app/src/main/java/com/example/intent5/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.intent5;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button Bt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Bt = findViewById(R.id.btnid);
+        Bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Second.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
